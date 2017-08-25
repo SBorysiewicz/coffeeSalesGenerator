ef77aa2faf90e0ffd994bd1f6f309aa344ff6339
Simon Borysiewicz <simon.borysiewicz@gmail.com>
2017-08-25 21:58:22 +1000

    2#: Added growth factor of BYO cups

--- src/CoffeeShops.js
+++ src/CoffeeShops.js
@@ -17,27 +17,4 @@ const shops = () => {
     return coffeeShops;
 };
 
-const buildShop = (name, suburb, state, incentive) => {
-
-    const averageSales = Math.ceil(Math.random() * (maxSales - minSales) + minSales);
-    return {
-        'name': name,
-        'suburb': suburb,
-        'state': state,
-        'incentive': incentive ? 'y' : 'n',
-        'monthlySales': () => {
-            let salesVariance = Math.ceil(Math.random() * (averageSales - (averageSales * 0.01)) + averageSales * 0.01) 
-            * (Math.random() < 0.5 ? -1 : 1);
-            console.log("averageSales: " + averageSales + " ,salesVariance: " + salesVariance);
-            return averageSales + salesVariance;
-        },
-        'monthlyOwnCups': (sales) => {
-            const factor = incentive ? 0.1 : 0.02;
-            const cupsMin = sales * 0.5 * factor;
-            const cupsMax = sales * factor;
-            return Math.ceil(Math.random() * (cupsMax - cupsMin) + cupsMin);
-        }
-    }
-};
-
 export default shops;
